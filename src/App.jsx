0ff27bc9c1163393cdca1826df17a009c02b3155
xoxwgys56 <lololtoday@gmail.com>
2022-02-13 08:28:14 +0900

    chore: apply firebase
    header is not fully updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import {Layout} from "antd";
-import './App.css';
-import "antd/dist/antd.min.css";
+import {initializeApp} from 'firebase/app';
+
 import Router from "./routes/Router";
 import Header from './components/Header';
+
+import './App.css';
+import "antd/dist/antd.min.css";
 
 const {Content: ContentWrapper, Footer: FooterWrapper} = Layout
 
 const App = () => {
   // TODO refactoring routing using nested routing
   // https://reactrouter.com/docs/en/v6/getting-started/tutorial#nested-routes
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyDodKEG9Mo5n4pS6w8ffuYEA3g7YGnCzJI",
+        authDomain: "starbucks-custom-beverage.firebaseapp.com",
+        databaseURL: "https://starbucks-custom-beverage-default-rtdb.firebaseio.com",
+        projectId: "starbucks-custom-beverage",
+        storageBucket: "starbucks-custom-beverage.appspot.com",
+        messagingSenderId: "106120833673",
+        appId: "1:106120833673:web:290ec334fa8e00e552f458",
+        measurementId: "G-KTMPKNH01G"
+    }
+    const app = initializeApp(firebaseConfig);
 
   return (
       <Layout>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,106 @@
-import React from "react";
+import React, {useState, useCallback} from "react";
 import {PageHeader, Button} from "antd";
 import {CoffeeOutlined} from '@ant-design/icons';
+import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
 
 
 const Header = () => {
     // TODO add signedIn prop
     // TODO highlight when enter `about` page
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    const [user, setUser] = useState(window.sessionStorage.getItem("user"));
+
+    const provider = new GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    const onSignIn = useCallback(() => {
+        const auth = getAuth();
+        signInWithPopup(auth, provider).
+            then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+
+                return {token, user};
+        }).then(({token, user}) => {
+            // console.log(token);
+            console.log(user);
+            // TODO add validation
+            if (user && token) {
+                // TODO update state
+                setIsSignedIn(true);
+                setUser({
+                    email: user.email,
+                    name: user.displayName,
+                    photoURL: user.photoURL,
+                    token: token
+                })
+                console.log(`update user ${user}`)
+                storeUserInfo(user);
+            }else {
+                console.log('Failed signed in.')
+            }
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            // const email = error.mail;
+            // const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(`Has error with ${errorCode}. see detail message ${errorMessage}.`)
+        })
+    });
+
+    const storeUserInfo = useCallback((user) => {
+        window.sessionStorage.setItem("user", JSON.stringify(user))
+    })
+
+    console.log(user)
+
+    const extraButtons = isSignedIn ? [
+        <Button
+            key="3"
+            type="text"
+            siz="large"
+            href="/pages/about"
+        >
+            About
+        </Button>,
+        <Button
+            key="2"
+            type="text"
+            siz="large"
+            href="/pages/register-beverage"
+        >
+            추가하기
+        </Button>,
+        <Button
+            key="1"
+            type="text"
+            href="/pages/mypage"
+        >
+            <img
+                src={user.photoURL}
+                style={{borderRadius:50, width:30}}
+            />
+        </Button>,
+    ] :[
+        <Button
+            key="2"
+            type="text"
+            siz="large"
+            href="/pages/about"
+        >
+            About
+        </Button>,
+        <Button
+            key="1"
+            type="primary"
+            style={{backgroundColor: '#237804', borderRadius: 5}}
+            size="large"
+            onClick={onSignIn}
+        >
+            Log in
+        </Button>,
+    ]
 
     return (<PageHeader
         className="site-page-header"
@@ -16,24 +111,7 @@ const Header = () => {
                     href="/"
                     style={{color: '#237804'}}
                 />}
-        extra={[
-            <Button
-                key="2"
-                type="text"
-                siz="large"
-                href="/pages/about"
-            >
-                About
-            </Button>,
-            <Button
-                key="1"
-                type="primary"
-                style={{backgroundColor: '#237804', borderRadius: 5}}
-                size="large"
-            >
-                Log in
-            </Button>,
-        ]}
+        extra={extraButtons}
         style={{backgroundColor: '#fff'}}
     />)
 };
