0b6e3bf9efe449367b6b644693c39729497b7af1
xoxwgys56 <lololtoday@gmail.com>
2022-02-24 01:14:21 +0900

    feat: add auth provider
    for managing user auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {initializeApp} from 'firebase/app';
 
 import Router from "./routes/Router";
 import Header from './components/Header';
-
+import AuthProvider from "./providers/AuthProvider";
 import './App.css';
 import "antd/dist/antd.min.css";
 
@@ -14,6 +14,7 @@ const App = () => {
   // TODO refactoring routing using nested routing
   // https://reactrouter.com/docs/en/v6/getting-started/tutorial#nested-routes
 
+    // TODO refactor below values
     const firebaseConfig = {
         apiKey: "AIzaSyDodKEG9Mo5n4pS6w8ffuYEA3g7YGnCzJI",
         authDomain: "starbucks-custom-beverage.firebaseapp.com",
@@ -24,14 +25,16 @@ const App = () => {
         appId: "1:106120833673:web:290ec334fa8e00e552f458",
         measurementId: "G-KTMPKNH01G"
     }
-    const app = initializeApp(firebaseConfig);
+    initializeApp(firebaseConfig);
 
   return (
       <Layout>
-        <Header/>
-        <ContentWrapper>
-          <Router/>
-        </ContentWrapper>
+          <AuthProvider>
+              <Header/>
+              <ContentWrapper>
+                  <Router/>
+              </ContentWrapper>
+          </AuthProvider>
         <FooterWrapper>footer</FooterWrapper>
       </Layout>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,27 @@
-import React, {useState, useCallback} from "react";
+import React from "react";
 import {PageHeader, Button} from "antd";
 import {CoffeeOutlined} from '@ant-design/icons';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
+import {AuthContext} from "../providers/AuthProvider";
+import useSignIn from "../hooks/useSignIn";
 
 
 const Header = () => {
     // TODO add signedIn prop
     // TODO highlight when enter `about` page
-    const [isSignedIn, setIsSignedIn] = useState(false);
-    const [user, setUser] = useState(window.sessionStorage.getItem("user"));
+    const {user} = React.useContext(AuthContext);
+    const onSignIn = useSignIn()
 
-    const provider = new GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    const onSignIn = useCallback(() => {
-        const auth = getAuth();
-        signInWithPopup(auth, provider).
-            then((result) => {
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                const user = result.user;
+    // temp user
+    let tempUser;
+    if (!user && localStorage.getItem('email')) tempUser = {
+        email: localStorage.getItem('email'),
+        name: localStorage.getItem('name'),
+        photoURL: localStorage.getItem('photoURL'),
+        token: localStorage.getItem('token')
+    }
+    else tempUser = user
 
-                return {token, user};
-        }).then(({token, user}) => {
-            // console.log(token);
-            console.log(user);
-            // TODO add validation
-            if (user && token) {
-                // TODO update state
-                setIsSignedIn(true);
-                setUser({
-                    email: user.email,
-                    name: user.displayName,
-                    photoURL: user.photoURL,
-                    token: token
-                })
-                console.log(`update user ${user}`)
-                storeUserInfo(user);
-            }else {
-                console.log('Failed signed in.')
-            }
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-
-            // const email = error.mail;
-            // const credential = GoogleAuthProvider.credentialFromError(error);
-            console.log(`Has error with ${errorCode}. see detail message ${errorMessage}.`)
-        })
-    });
-
-    const storeUserInfo = useCallback((user) => {
-        window.sessionStorage.setItem("user", JSON.stringify(user))
-    })
-
-    console.log(user)
-
-    const extraButtons = isSignedIn ? [
+    const extraButtons = user || tempUser ? [
         <Button
             key="3"
             type="text"
@@ -78,8 +44,9 @@ const Header = () => {
             href="/pages/mypage"
         >
             <img
-                src={user.photoURL}
+                src={tempUser.photoURL}
                 style={{borderRadius:50, width:30}}
+                alt="user profile image"
             />
         </Button>,
     ] :[
